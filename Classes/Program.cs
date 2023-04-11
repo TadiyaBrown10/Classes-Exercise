0af661f8e40fb4256520d0fa8d9785fa670908ae
Tadiya Brown <tadiyabrown2000@gmail.com>
2023-04-11 11:14:43 -0400

    classes completed

---
+++ Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var newRide = new Car();
+
+            newRide.Make = "Jeep";
+
+            newRide.Model = "Wrangler";
+
+            newRide.Year = 2020;
+
+            var Ford = new Car()
+            {
+                Make = "Ford",
+                Model = "F150",
+                Year = 2018
+
+            };
+
+            var carList = new List<Car> { newRide, Ford };
+
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
+        }
+    }
+}
